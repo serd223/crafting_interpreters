14adae00c53593c1f636c37a3676d703a0128876
serd <patatd@outlook.com.tr>
2023-03-29 21:39:11 +0300

    Recognizing Lexemes (4.5) done

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,8 +3,8 @@ use super::token::*;
 pub struct Scanner {
     source: String,
     tokens: Vec<Token>,
-    start: u32,
-    current: u32,
+    start: usize,
+    current: usize,
     line: u32,
 }
 
@@ -20,10 +20,41 @@ impl Scanner {
     }
 
     fn is_at_end(&self) -> bool {
-        self.current as usize >= self.source.len()
+        self.current >= self.source.len()
     }
 
-    fn scan_token(&mut self) {}
+    fn scan_token(&mut self) {
+        let c = self.advance();
+
+        match c {
+            '(' => self.add_token(TokenType::LeftParen, None),
+            ')' => self.add_token(TokenType::RightParen, None),
+            '{' => self.add_token(TokenType::LeftBrace, None),
+            '}' => self.add_token(TokenType::RightBrace, None),
+            ',' => self.add_token(TokenType::Comma, None),
+            '.' => self.add_token(TokenType::Dot, None),
+            '-' => self.add_token(TokenType::Minus, None),
+            '+' => self.add_token(TokenType::Plus, None),
+            ';' => self.add_token(TokenType::Semicolon, None),
+            '*' => self.add_token(TokenType::Star, None),
+            _ => (),
+        }
+    }
+
+    fn advance(&mut self) -> char {
+        self.current += 1;
+        self.source.chars().nth(self.current).unwrap()
+    }
+
+    fn add_token(&mut self, token_type: TokenType, literal: Option<String>) {
+        let text = self.source.get(self.start..self.current).unwrap();
+        self.tokens.push(Token {
+            token_type,
+            lexeme: text.to_string(),
+            literal,
+            line: self.line,
+        });
+    }
 
     pub fn scan_tokens(&mut self) -> Vec<Token> {
         while !self.is_at_end() {
